1e4914d9cee1fba75e5234a0bb0d396bd62ab369
ringring1 <71864538+ringring1@users.noreply.github.com>
2020-09-25 14:20:26 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,13 +10,35 @@ install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 # Remove temporary directory
 rm -rf /tmp/v2ray
 
+mkdir /tmp/caddy
+curl -L -H "Cache-Control: no-cache" -o /tmp/caddy/caddy.tar.gz --insecure https://github.com/caddyserver/caddy/releases/download/v1.0.3/caddy_v1.0.3_linux_amd64.tar.gz
+tar -zxvf /tmp/caddy.tar.gz caddy
+install -m 755 /tmp/caddy/caddy /usr/local/bin/caddy
+mkdir /usr/local/wwwroot
+wget -O /usr/share/wwwroot/index.html https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
+rm -rf /tmp/caddy
+
+install -d /usr/local/caddy
+cat << EOF > /usr/local/caddy/Caddyfile
+http://0.0.0.0:$PORT
+{
+	root /wwwroot
+	index index.html
+	timeouts none
+	proxy /ws 127.0.0.1:9090 {
+		websocket
+		header_upstream -Origin
+	}
+}
+EOF
+
 # V2Ray new configuration
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
-            "port": $PORT,
+            "port": 9090,
             "protocol": "vmess",
             "settings": {
                 "clients": [
@@ -42,3 +64,4 @@ EOF
 
 # Run V2Ray
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/local/bin/caddy --conf=/usr/local/caddy/Caddyfile
